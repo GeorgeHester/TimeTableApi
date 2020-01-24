f81e24c8eb0ea9ff495857754079e877fbb9af51
GeorgeHester <48884343+GeorgeHester@users.noreply.github.com>
2020-01-24 19:39:26 +0000

    Update app.js

--- app.js
+++ app.js
@@ -95,14 +95,13 @@ app.post('/xlsx', async (req, res) => {
 
     editxlsx(data);
 
-    res.sendFile('./xlsx/temp.xlsx');
-        /*, `${req.body.name} - TimeTable 2020 GCSE.xlsx`, (err) => {
+    res.download('./xlsx/temp.xlsx', `${req.body.name} - TimeTable 2020 GCSE.xlsx`, (err) => {
         if (err) {
             console.log(err);
             res.status(500);
             return;
         };
-    });*/
+    });
 });
 
 app.listen(process.env.PORT || 3000, function () {
