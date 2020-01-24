db77ae4aae2f451da141e667c5cc662fe7aeb1db
GeorgeHester <48884343+GeorgeHester@users.noreply.github.com>
2020-01-24 20:49:32 +0000

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.post('/xlsx', async (req, res) => {
 
     editxlsx(data, req.ip);
 
-    res.status(200).send(`./xlsx/temp-${ip}.xlsx`);
+    res.send(`./xlsx/temp-${ip}.xlsx`);
         /*.download('./xlsx/temp.xlsx', `${req.body.name} - TimeTable 2020 GCSE.xlsx`, (err) => {
         if (err) {
             console.log(err);
