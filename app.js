4f52dba6e81d8051758e37225bfc180db23f0c42
GeorgeHester <48884343+GeorgeHester@users.noreply.github.com>
2020-01-24 19:36:47 +0000

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.post('/xlsx', async (req, res) => {
 
     editxlsx(data);
 
-    res.send('./xlsx/temp.xlsx', `${req.body.name} - TimeTable 2020 GCSE.xlsx`, (err) => {
+    res.sendFile('./xlsx/temp.xlsx', `${req.body.name} - TimeTable 2020 GCSE.xlsx`, (err) => {
         if (err) {
             console.log(err);
             res.status(500);
